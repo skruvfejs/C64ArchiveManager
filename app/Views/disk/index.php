315be43e56c78aef130985214b658c793baaf2e0
screwface <skruvfejs@proton.me>
2026-08-01 22:20:47 +0200

    Show disk metadata in Disk Explorer

--- app/Views/disk/index.php
+++ app/Views/disk/index.php
@@ -1,54 +1,105 @@
 <h1><?= $title ?></h1>
 
+
 <h2>
     Release:
     <?= htmlspecialchars($release->getName()) ?>
 </h2>
 
+
 <?php foreach ($files as $file): ?>
 
-    <h3>
-        <?= htmlspecialchars($file->getFilename()) ?>
-        (<?= htmlspecialchars($file->getFormat()) ?>)
-    </h3>
+    <div>
 
-    <table border="1" cellpadding="5">
+        <h3>
+            <?= htmlspecialchars($file->getFilename()) ?>
+            (<?= htmlspecialchars($file->getFormat()) ?>)
+        </h3>
 
-        <tr>
-            <th>Filename</th>
-            <th>Type</th>
-            <th>Track</th>
-            <th>Sector</th>
-            <th>Blocks</th>
-        </tr>
 
-        <?php foreach ($directories[$file->getId()] ?? [] as $entry): ?>
+        <table border="1" cellpadding="5">
 
             <tr>
+                <th>Disk name</th>
                 <td>
-                    <?= htmlspecialchars($entry->getFilename()) ?>
-                </td>
-
-                <td>
-                    <?= htmlspecialchars($entry->getFiletype()) ?>
-                </td>
-
-                <td>
-                    <?= $entry->getStartTrack() ?>
-                </td>
-
-                <td>
-                    <?= $entry->getStartSector() ?>
-                </td>
-
-                <td>
-                    <?= $entry->getBlocks() ?>
+                    <?= htmlspecialchars(
+                        $file->getDiskName() ?? ''
+                    ) ?>
                 </td>
             </tr>
 
-        <?php endforeach; ?>
+            <tr>
+                <th>Disk ID</th>
+                <td>
+                    <?= htmlspecialchars(
+                        $file->getDiskId() ?? ''
+                    ) ?>
+                </td>
+            </tr>
 
-    </table>
+            <tr>
+                <th>Format</th>
+                <td>
+                    <?= htmlspecialchars(
+                        $file->getFormat()
+                    ) ?>
+                </td>
+            </tr>
+
+        </table>
+
+
+        <br>
+
+
+        <table border="1" cellpadding="5">
+
+            <tr>
+                <th>Filename</th>
+                <th>Type</th>
+                <th>Track</th>
+                <th>Sector</th>
+                <th>Blocks</th>
+            </tr>
+
+
+            <?php foreach ($directories[$file->getId()] ?? [] as $entry): ?>
+
+                <tr>
+
+                    <td>
+                        <?= htmlspecialchars(
+                            $entry->getFilename()
+                        ) ?>
+                    </td>
+
+                    <td>
+                        <?= htmlspecialchars(
+                            $entry->getFiletype()
+                        ) ?>
+                    </td>
+
+                    <td>
+                        <?= $entry->getStartTrack() ?>
+                    </td>
+
+                    <td>
+                        <?= $entry->getStartSector() ?>
+                    </td>
+
+                    <td>
+                        <?= $entry->getBlocks() ?>
+                    </td>
+
+                </tr>
+
+            <?php endforeach; ?>
+
+
+        </table>
+
+    </div>
+
 
 <?php endforeach; ?>
 
